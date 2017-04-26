e92c8e4f75c3c701701889cc0f45f5d7de045c1d
Karl Xu <karl.yogi.xu@gmail.com>
2017-04-26 18:50:00 +0800

    extract router

--- src/pages/hello.php
+++ src/pages/hello.php
@@ -1,5 +1,4 @@
-<?php
-
+<?php 
 $input = $request->get('name', 'World');
 
-$response->setContent(sprintf('Hello %s', htmlspecialchars($input, ENT_QUOTES, 'UTF-8')));
+echo(sprintf('Hello %s', htmlspecialchars($input, ENT_QUOTES, 'UTF-8')));
