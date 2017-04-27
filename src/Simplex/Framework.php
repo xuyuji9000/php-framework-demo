43b7a9f8c85e553301a834bc6fd87f7d38256040
Karl Xu <karl.yogi.xu@gmail.com>
2017-04-27 16:49:57 +0800

    unit-test

--- src/Simplex/Framework.php
+++ src/Simplex/Framework.php
@@ -4,10 +4,13 @@ namespace Simplex;
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Matcher\UrlMatcher;
 use Symfony\Component\HttpKernel\Controller\ArgumentResolver;
 use Symfony\Component\HttpKernel\Controller\ControllerResolver;
+use Symfony\Component\Routing\Matcher\UrlMatcherInterface;
+use Symfony\Component\HttpKernel\Controller\ArgumentResolverInterface;
+use Symfony\Component\HttpKernel\Controller\ControllerResolverInterface;
 use Symfony\Component\Routing\Exception\ResourceNotFoundException;
-use Symfony\Component\Routing\Matcher\UrlMatcher;
 
 
 class Framework
@@ -16,7 +19,7 @@ class Framework
     protected $controllerResolver;
     protected $argumentResolver;
 
-    public function __construct(UrlMatcher $matcher, ControllerResolver $controllerResolver, ArgumentResolver $argumentResolver)
+    public function __construct(UrlMatcherInterface $matcher, ControllerResolverInterface $controllerResolver, ArgumentResolverInterface $argumentResolver)
     {
         $this->matcher = $matcher;
         $this->controllerResolver = $controllerResolver;
@@ -34,7 +37,7 @@ class Framework
             $arguments = $this->argumentResolver->getArguments($request, $controller);
         
             $response = call_user_func_array($controller, $arguments);
-        } catch (Routing\Exception\ResourceNotFoundException $e) {
+        } catch (ResourceNotFoundException $e) {
             $response = new Response('Not Found', 404);
         } catch (Exception $e) {
             $response = new Response($e->getMessage(), 500);
